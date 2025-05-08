a50fd2686380d6a5ea4f21ce15b1fc1a0eb50c75
Utitofon-Udoekong <utitofonudoekong0@gmail.com>
2025-05-08 12:59:18 +0100

    Update .gitignore to exclude all .env files except .env.example, and refactor AuthPage to use environment variable for redirect URL, enhancing configuration flexibility.

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -10,14 +10,14 @@ export default function AuthPage() {
   
   const router = useRouter();
   const supabase = createClient();
-
+  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
   const handleSignInWithGoogle = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setLoading(true);
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'http://localhost:3000/api/auth/callback',
+        redirectTo: `${appUrl}/api/auth/callback`,
       },
     })
     if (error) {
