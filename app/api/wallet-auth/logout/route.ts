d5729e7a6c4b08c935d43e2d1dde888cace583a1
Utitofon-Udoekong <utitofonudoekong0@gmail.com>
2025-06-26 17:46:12 +0100

    Refactor logout API and remove unused dashboard components
    - Updated the logout API to return a JSON response with a message and improved cookie handling for better security.
    - Removed several unused components from the dashboard, including ArticleCard, AudioCard, EmptyState, ScheduledContentCard, and VideoCard, to streamline the codebase and enhance maintainability.

--- app/api/wallet-auth/logout/route.ts
+++ app/api/wallet-auth/logout/route.ts
@@ -1,18 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-import cookie from "cookie";
 
 import { cookieName } from "@/app/utils/supabase";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
-  _req: NextRequest,
-  _res: NextResponse
+  req: NextRequest, 
 ) {
 
-  return new Response('Logged out', {
-    status: 200,
-    headers: { 'Set-Cookie': cookie.serialize(cookieName, "", {
-      path: "/",
-      maxAge: -1,
-    }) },
-  })
+  const response = NextResponse.json({
+    message: "Logged out"
+  });
+
+  response.cookies.set(cookieName, "", {
+    path: "/",
+    secure: process.env.NODE_ENV === "production", // Only HTTPS in production
+    httpOnly: true, // ✅ Prevent XSS attacks
+    sameSite: "strict", // ✅ Prevent CSRF attacks
+    maxAge: 0,
+  });
+
+  return response;
+
 }
